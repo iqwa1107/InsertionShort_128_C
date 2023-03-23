53ab58e44118e9c502faef4b65a9ea8044279acc
iqwa1107 <ramahs49@gmail.com>
2023-03-23 14:50:03 +0700

    membuat variable inputan n

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int arr[20]; //mrmbuat array dengan panjang data 20
+int n;       //membuat variable inputan n
+
+void input()
+{  // produk input
+	while (true)
+	{ 
+		cout << "memasukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20)
+		{ //membuat kondisi n tidak lebih dari 20
+			break;
+
+		}
+		else
+		{
+			cout << "\nArray yang anda masukan maksi al 20 elemen. \n";   //menampilkan pesan jika data  
+		}
+
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
